c161e260e78fc758b92e13aae1433454fdf0d2bc
marcelohbb <marcelo_aut@hotmail.com>
2020-05-26 15:29:21 -0400

    subindo aplicação

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -18,16 +18,54 @@ interface IRequest {
 }
 
 @injectable()
-class CreateOrderService {
+class CreateProductService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customer = await this.customersRepository.findById(customer_id);
+
+    if (!customer) throw new AppError('User not found.');
+
+    const productsId = products.map(product => {
+      const { id } = product;
+      return { id };
+    });
+
+    const productsInfo = await this.productsRepository.findAllById(productsId);
+
+    const parsedProducts = products.map(product => {
+      const productIndex = productsInfo.findIndex(
+        productInfo => productInfo.id === product.id,
+      );
+
+      if (productsInfo[productIndex].quantity < product.quantity)
+        throw new AppError('Invalid balance of product to post order');
+
+      const product_id = product.id;
+      const { price } = productsInfo[productIndex];
+      const { quantity } = product;
+
+      return { product_id, price, quantity };
+    });
+
+    const order = await this.ordersRepository.create({
+      customer,
+      products: parsedProducts,
+    });
+
+    await this.productsRepository.updateQuantity(products);
+
+    return order;
   }
 }
 
-export default CreateOrderService;
+export default CreateProductService;
